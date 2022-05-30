101ebbf97a0d933b1ab046c9cbe6e8837b721a35
qinyq <hellolutar@163.com>
2022-05-30 15:13:43 +0800

    add Judgment of success and failure

--- eureka-server/docker/invoke.sh
+++ eureka-server/docker/invoke.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 #######################################自定义变量
 JAR_PORT=32000
 DOCKER_REPO_URI=172.25.112.27:31001
@@ -9,11 +8,12 @@ NAME=$(mvn help:evaluate "-Dexpression=project.name" | grep "^[^\[]")
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep "^[^\[]")
 PORT_MAPPING="$JAR_PORT:$JAR_PORT"
 VOLUME_MAPPING="/var/log/$NAME:/opt/app/logs"
+SUCCESS_FLAG=0
 #######################################自定义变量end
 
 #生成Dockerfile文件
 function generateDockerfile() {
-  cat >>Dockerfile <- EOF
+  cat >>docker/Dockerfile <- EOF
   FROM openjdk:8u332-jre
   RUN mkdir /opt/app
   ADD target/$NAME-$VERSION.jar /opt/app/app.jar
@@ -23,11 +23,24 @@ function generateDockerfile() {
   EXPOSE $JAR_PORT
   EOF
 
+  if [ -f "docker/Dockerfile" ]; then
+    SUCCESS_FLAG=$SUCCESS_FLAG+1
+  else
+    SUCCESS_FLAG=$SUCCESS_FLAG-1
+  fi
+
 }
 
 function dockerLogin() {
   docker logout
   docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWD $DOCKER_REPO_URI
+
+  if [ $(echo $?) -gt 0 ]; then
+       echo "docker login SUCCESS"
+    else
+       echo "docker login FAILD"
+       return 1
+  fi
 }
 
 ## 构建docker镜像
@@ -36,6 +49,12 @@ function buildImage() {
   echo "build image ..."
   docker build -t $DOCKER_IMAGE -f docker/Dockerfile
 
+  if [ $(echo $?) -gt 0 ]; then
+     echo "build docker image SUCCESS"
+  else
+     echo "build docker image FAILD"
+     return 1
+  fi
 }
 
 ## 推送docker镜像到仓库
@@ -44,6 +63,13 @@ function pushImageToRepo() {
   echo "pushing image ..."
   dockerLogin
   docker push $DOCKER_IMAGE
+
+  if [ $(echo $?) -gt 0 ]; then
+       echo "push image of docker to repo  SUCCESS"
+  else
+       echo "push image of docker to repo FAILD"
+       return 1
+  fi
 
 }
 
@@ -56,6 +82,13 @@ function runContainer() {
 
   echo "docker run image ..."
   docker run $DOCKER_IMAGE --name $NAME -p $PORT_MAPPING -v $VOLUME_MAPPING
+
+  if [ $(echo $?) -gt 0 ]; then
+         echo "run image of docker to repo  SUCCESS"
+  else
+         echo "run image of docker to repo FAILD"
+         return 1
+  fi
 }
 
 function main() {
